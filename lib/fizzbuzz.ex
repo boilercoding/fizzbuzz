53f159985c722dab58b2d29d7379f7a88a1b6a20
boilercoding <boilercoding@gmail.com>
2017-08-01 20:33:14 -0400

    Adds fizzbuzz map

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -1,18 +1,10 @@
 defmodule Fizzbuzz do
-  @moduledoc """
-  Documentation for Fizzbuzz.
-  """
+  def fizzbuzz(n) when rem(n, 15) == 0, do: "FizzBuzz"
+  def fizzbuzz(n) when rem(n, 05) == 0, do: "Buzz"
+  def fizzbuzz(n) when rem(n, 03) == 0, do: "Fizz"
+  def fizzbuzz(n),                                    do: n
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Fizzbuzz.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def result(n) do
+    Enum.map(1..n, fn(n) -> {n , fizzbuzz(n)} end) |> Map.new
   end
 end
